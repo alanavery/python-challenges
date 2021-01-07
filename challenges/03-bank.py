b7b961bf64ae374dbfaf6e08ca92330e65a7668b
delayedaa <delayedaa@gmail.com>
2021-01-07 14:53:29 -0600

    Complete challenges 1-4

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,18 @@
-print("Welcome to Chase bank.")
-print("Have a nice day!")
+balance = 100
 
+print("Welcome to Chase bank.")
+print('Your current balance is')
+print(balance)
+direction = input(
+    'What would you like to do? (deposit, withdraw, check_balance)\n')
+if direction == 'deposit':
+    deposit_num = input('How much do you want to deposit?\n')
+    balance += int(deposit_num)
+    print(balance)
+elif direction == 'withdraw':
+    withdraw_num = input('How much do you want to withdraw?\n')
+    balance -= int(withdraw_num)
+    print(balance)
+else:
+    print(balance)
+print("Have a nice day!")
--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,11 @@
-# You'll need to use a couple of built in functions to alphabetize a string. 
+# You'll need to use a couple of built in functions to alphabetize a string.
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+
+def string_sort(string):
+    b = sorted(string)
+    c = ''.join(b)
+    print(c)
+
+
+string_sort('string')
